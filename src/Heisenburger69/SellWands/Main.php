b2b10b80139862fd39e945f3fa36a8e9b925a0bd
Heisenburger69 <apkforgss@gmail.com>
2020-03-08 13:34:52 +0530

    Revamp ree

--- src/Heisenburger69/SellWands/Main.php
+++ src/Heisenburger69/SellWands/Main.php
@@ -4,47 +4,43 @@ declare(strict_types=1);
 
 namespace Heisenburger69\SellWands;
 
+use onebone\economyapi\EconomyAPI;
 use pocketmine\block\Block;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\item\Item;
 use pocketmine\math\Vector3;
 use pocketmine\nbt\tag\StringTag;
 use pocketmine\plugin\PluginBase;
-use pocketmine\command\CommandSender;
-use pocketmine\command\Command;
-use pocketmine\item\Item;
-use onebone\economyapi\EconomyAPI;
 use pocketmine\tile\Chest;
-use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
-use pocketmine\Player;
 
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase implements Listener
+{
 
     public $cfg;
 
-    public function onEnable() : void{
+    public function onEnable(): void
+    {
 
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->saveDefaultConfig();
         $this->cfg = $this->getConfig()->getAll();
     }
-    public function replaceVars($str, array $vars) : string{
-        foreach($vars as $key => $value){
-            $str = str_replace("{" . $key . "}", $value, $str);
-        }
-        return $str;
-    }
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-        switch($command->getName()){
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+    {
+        switch ($command->getName()) {
             case "sellwand":
-                if(!$sender->hasPermission("sellwand.command")){
+                if (!$sender->hasPermission("sellwand.command")) {
                     $sender->sendMessage(TextFormat::colorize($this->msg["error.permission"]));
                     return true;
                 }
-                if(isset($args[0])) {
+                if (isset($args[0])) {
                     $player = $this->getServer()->getPlayer($args[0]);
-                    if($player === null) {
+                    if ($player === null) {
                         $sender->sendMessage(TextFormat::RED . "Player not found!");
                         return true;
                     }
@@ -60,11 +56,15 @@ class Main extends PluginBase implements Listener {
         }
     }
 
+    /**
+     * @param PlayerInteractEvent $event
+     * 
+     */
     public function onInteract(PlayerInteractEvent $event)
     {
         if ($event->getAction() === PlayerInteractEvent::RIGHT_CLICK_BLOCK) {
             $player = $event->getPlayer();
-            if(!$player->hasPermission("sellwand.use")) {
+            if (!$player->hasPermission("sellwand.use")) {
                 return;
             }
             $item = $event->getItem();
@@ -92,11 +92,10 @@ class Main extends PluginBase implements Listener {
                         if ($revenue <= 0) {
                             $player->sendMessage(TextFormat::RED . "There are no items to sell in this Chest");
                             $event->setCancelled(true);
-                            return true;
+                            return;
                         }
                         EconomyAPI::getInstance()->addMoney($player->getName(), (int)$revenue);
-                        $player->sendMessage(TextFormat::colorize($this->replaceVars("&a&lSuccess! &r&7sold the contents of the Chest for §8\${MONEY}", array(
-                            "MONEY" => (string)$revenue))));
+                        $player->sendMessage(TextFormat::colorize(str_replace("{MONEY}", $revenue, "&a&lSuccess! &r&7sold the contents of the Chest for §8\${MONEY}")));
                         $event->setCancelled(true);
                     }
                 }
